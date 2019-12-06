dc3e72588b9d53bcac64aa3249329b9165ad255b
Pratham Murkute <58584701+pratham654@users.noreply.github.com>
2019-12-06 12:44:37 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,25 @@
+/**
+ * Copyright (C) 2019
+ * All rights reserved.
+ *
+ * main.c
+ *
+ *  Created on: 06-Dec-2019
+ *      Author: Pratham
+ */
+ 
+/* header includes */
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/* main function */
+int main(void) {
+	/* print message */
+	printf("\r\nHello World ...");
+	
+	/* function exit */
+	return 0;
+}
+
+/************************* end of source file *************************/
